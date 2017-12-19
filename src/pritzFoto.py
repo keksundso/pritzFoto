a0921bc5c3f02166f789c120f4fa83516ad6c065
keks <keksundso@hotmail.de>
2017-12-19 18:36:52 +0100

    gui gebaut und weises feld hinzugefuegt

--- src/pritzFoto.py
+++ src/pritzFoto.py
@@ -25,6 +25,13 @@ def copyRenameBurn(old_file_name, new_file_name,textBurn):
 
     
     img = Image.open(src_file)
+    
+
+    back = Image.new("RGB", [img.size[0],img.size[1]+100], "black")
+    back.paste(img,[0,100])
+
+    img = back
+
     draw = ImageDraw.Draw(img)
 
     fonttype = os.path.join(rootDic,"src","abel-regular.ttf")
@@ -32,13 +39,12 @@ def copyRenameBurn(old_file_name, new_file_name,textBurn):
 
     font = ImageFont.truetype(fonttype, fontSize)
     draw.text((0, 0),textBurn,(255,255,255),font=font)
-    img.save(new_dst_file_name)
 
-        
+    img.save(new_dst_file_name)
 
 
 def scanFotoFolder(fotoFolderPath,sufix):
-    
+    abbruch = False
     listOfFiles = []
     for file in os.listdir(fotoFolderPath):
         if file.endswith("."+str(sufix)):
@@ -46,8 +52,10 @@ def scanFotoFolder(fotoFolderPath,sufix):
 
     if len(listOfFiles) == 0:
         print "Kein neues Bild gefunden"
+        abbruch = True
     elif len(listOfFiles) == 1:
         choosenIndex = 0
+
 
     else:
         print ""
@@ -65,22 +73,26 @@ def scanFotoFolder(fotoFolderPath,sufix):
             try:
                 choosenIndex = int(choosenIndex)
             except:
+                if choosenIndex == "c" or choosenIndex == "C":
+                    abbruch = True
+                    choosenIndex = -1
                 print "Es muss eine ganze Zahl eingegeben werden!!!"
         choosenIndex = choosenIndex-1
         print ""
 
-    frage = str(listOfFiles[choosenIndex])+" gefunden, um welche Kennziffer handelt es sich?: "
-    kn = raw_input(frage)  
+    if abbruch == False:
+        frage = str(listOfFiles[choosenIndex])+" gefunden, um welche Kennziffer handelt es sich?: "
+        kn = raw_input(frage)  
 
-    now = datetime.datetime.now()
-    iD = "KN"+str(kn)+"_"+str(now).split(".")[0].replace(" ","_").replace(":","-")
-    copyRenameBurn(listOfFiles[choosenIndex] ,(iD+".png"),str(iD))
-    shutil.move(os.path.join(rootDic, "newFotos",str(listOfFiles[choosenIndex])), os.path.join(rootDic, "oldFotos",str(listOfFiles[choosenIndex])))
+        now = datetime.datetime.now()
+        iD = "KN"+str(kn)+"_"+str(now).split(".")[0].replace(" ","_").replace(":","-")
+        copyRenameBurn(listOfFiles[choosenIndex] ,(iD+".png"),str(iD)+"\nTemperatur: 10C und Luftfeuchtigkeit 20%")
+        shutil.move(os.path.join(rootDic, "newFotos",str(listOfFiles[choosenIndex])), os.path.join(rootDic, "oldFotos",str(listOfFiles[choosenIndex])))
 
 
 def addEntryKnDic(knDic):
     # Kennziffer Nachschlagewrk
-    
+    abbruch = False
 
     nextKnSug =  sorted(knDic.keys())[-1]+1 # Suggestion for next kennziffer
 
@@ -98,9 +110,13 @@ def addEntryKnDic(knDic):
                 showKnDic(knDic,newKn)
                 break
         except:
-            puts(colored.red(8*">"+" Kenziffern muessen ganze zahlen sein und auch wirklich nur Zahlen!, versuchs nochmal"))
-             
-    pickle.dump( knDic, open( "knDic.p", "wb" ) )
+            if newKn == "c" or newKn == "C":
+                abbruch = True
+                break
+            else:
+                puts(colored.red(8*">"+" Kenziffern muessen ganze zahlen sein und auch wirklich nur Zahlen!, versuchs nochmal"))
+    if abbruch == False:     
+        pickle.dump( knDic, open( "knDic.p", "wb" ) )
 
     return knDic
 
@@ -120,13 +136,51 @@ def showKnDic(knDic,highKn=None):
             puts(colored.green(begin*" "+str(n)).ljust(columnWidth)+"|  "+str(knDic[n]))
     print ""
 
+def give_gui():
+    import Tkinter as Tk
+    import tkFileDialog
+
+    def schiesen():
+        root.quit()
+
+    root = Tk.Tk()
+    root.wm_title("pritzFoto")
+
+    root.minsize(width=80, height=60)
+
+
+    showKnDicButton = Tk.Button(master = root,  text = 'Zeige Kennziffer',activebackground="red",  command=guiCallshowKnDic)
+    scanFotoFolderButton = Tk.Button(master = root, text = 'Scan newFoto Ordner' ,activebackground="red", command=guiCallscanFotoFolder)
+    closeButton = Tk.Button(master = root, text = 'Close',activebackground="red", command=schiesen)
+    addknDicButton = Tk.Button(master = root, text = 'Add Kennziffer',activebackground="red", command=guiCallknDic)
+ 
+
+    #Anordnung
+    #showKnDicButton.grid(row=3, column=0)
+    #scanFotoFolderButton.grid(row=4, column=0)
+    #closeButton.grid(row=5, column=0)
+
+    showKnDicButton.place(relx=0.5, rely=0.5, anchor="n")
+    addknDicButton.place(relx=0.5, rely=0.5, anchor="s")
+
+    scanFotoFolderButton.place(relx=0.5, rely=0.1, anchor="n")
+
+    closeButton.place(relx=0.5, rely=0.9, anchor="s")
+
+    Tk.mainloop()
+
+
 
 
 ####Function Calls
-#knDic = addEntryKnDic(pickle.load( open( "knDic.p", "rb" ) ))
-showKnDic(pickle.load( open( "knDic.p", "rb" ) ))
-#scanFotoFolder(os.path.join(rootDic, "newFotos"),"png")
+def guiCallknDic():
+    knDic = addEntryKnDic(pickle.load( open( "knDic.p", "rb" ) ))
+def guiCallshowKnDic():
+    showKnDic(pickle.load( open( "knDic.p", "rb" ) ))
 
+def guiCallscanFotoFolder():
+    scanFotoFolder(os.path.join(rootDic, "newFotos"),"png")
+give_gui()
 
 
 
--- src/test.py
+++ src/test.py
@@ -1,27 +1,10 @@
 from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw 
 
+src_file = r"C:\Users\keks\Dropbox\Programieren\pritzFoto\oldFotos\Zebra (2).png"
 
+img = Image.open(src_file)
 
-image = Image.open("C:\\Users\\keks\\Dropbox\\Programieren\\pritzFoto\\newFotos\\test.png")
-draw = ImageDraw.Draw(image)
-txt = "Hello World"
-fontsize = 1  # starting font size
+back = Image.new("RGB", [img.size[0],img.size[1]+100], "white")
+back.paste(img,[0,100])
 
-# portion of image width you want text width to be
-img_fraction = 0.50
-
-font = ImageFont.truetype("arial.ttf", fontsize)
-while font.getsize(txt)[0] < img_fraction*image.size[0]:
-    # iterate until the text size is just larger than the criteria
-    fontsize += 1
-    font = ImageFont.truetype("arial.ttf", fontsize)
-
-# optionally de-increment to be sure it is less than criteria
-fontsize -= 1
-font = ImageFont.truetype("arial.ttf", fontsize)
-
-print 'final font size',fontsize
-draw.text((10, 25), txt, font=font) # put the text on the image
-image.save("C:\\Users\\keks\\Dropbox\\Programieren\\pritzFoto\\newFotos\\test_out.png") # save it
+back.save(r"C:\Users\keks\Dropbox\Programieren\pritzFoto\oldFotos\Zebra (2)_out.png") 
